471e5a39e14c97e7b619e10dfa489c11fc86e90b
hyerimmy <heyrim2010@naver.com>
2022-07-01 01:40:12 +0900

    Initial Commit

---
+++ AppleTv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleTv
+//
+//  Created by 황혜림 on 2022/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
